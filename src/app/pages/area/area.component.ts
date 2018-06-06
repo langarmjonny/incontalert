e5e849b76616f13b2b721db27553f5bbf5de712f
nicolas <nicolas28@web.de>
2018-06-06 14:01:59 +0200

    area work

--- src/app/pages/area/area.component.ts
+++ src/app/pages/area/area.component.ts
@@ -7,18 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./area.component.scss']
 })
 export class AreaComponent implements OnInit {
-	left1 = 200;
-	top1 = 200;
+	left1 = 300;
+	top1 = 300;
 	top2 = 200;
 	left2 = 200; 
-	hidden1 = 'hidden';
-	hidden2 = 'hidden'; 
+	visible1 = false;
+	visible2 = false; 
 	point1: boolean = false;
 	point2: boolean = false;
+  cols = 40; 
+  rows = 20; 
+  rects = [];
+  values= [{color : "", value : -1},{color: "rgba(255, 0,0, 0.5)", value : 1},{color: "rgba(163, 255, 209, 0.5)", value : 2}];
+  difficulty= this.values[1];
+
   constructor() { }
 
   ngOnInit() {
+    this.generateRectArray(null);
   }
+
   setPoint(point){
   	if(1== point)
   		this.point1 = !this.point1;
@@ -32,15 +40,36 @@ export class AreaComponent implements OnInit {
   }
   changePosition(e){
   	if(this.point1) {
-  		this.hidden1 = 'visible';
-	  	this.left1 = parseInt(e.offsetX) +  parseInt(document.getElementById('x1').getAttribute('width'));
-	  	this.top1 =  parseInt(e.offsetY) +  parseInt(document.getElementById('x1').getAttribute('height')) ;
-	}
-	if(this.point2) {
-		this.hidden2 = 'visible';
-	  	this.left2 =  parseInt(e.offsetX) +  parseInt(document.getElementById('x2').getAttribute('width'));
-	  	this.top2 =  parseInt(e.offsetY) +  parseInt(document.getElementById('x2').getAttribute('height'));
-	}
-  	console.log(e); 
+  		this.visible1 = true;
+	  	this.left1 = e.offsetX -15 ;
+	  	this.top1 =  e.offsetY -15;
+     console.log(this.left1 + " " + this.top1);
+  	}
+  	if(this.point2) {
+  		this.visible2 = true;
+  	  	this.left2 =  e.offsetX -15;
+  	  	this.top2 =  e.offsetY -15 ;
+  	}
   }
+  generateRectArray(values){
+    this.rects = [];
+    for(var i= 0; i < this.rows; i++ )
+    {
+      for(var j=0; j < this.cols; j++)
+        {
+          this.rects.push({
+            x:Math.floor( j * 1500 / this.cols),
+            y:Math.floor( i * 702 / this.rows),
+            value: this.values[2],
+            width: Math.floor(1500 / this.cols),
+            height: Math.floor(702 / this.rows),
+          })
+        }
+    }
+    console.log(this.rects);
+  }
+  changeRectValue(rect){
+    rect.value = this.difficulty;  
+  }
+
 }
