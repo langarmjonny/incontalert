a33bebdf788a75b16187d8e504e9c1dc4da04193
admin_robotino <admin_robotino@BTAPD175.ad.uni-bayreuth.de>
2018-06-19 12:47:23 +0200

    commit

--- src/app/@core/data/dashboard.service.ts
+++ src/app/@core/data/dashboard.service.ts
@@ -6,13 +6,13 @@ import { of } from 'rxjs/observable/of';
 const httpOptions= {
 	headers: new HttpHeaders({
 		'Content-Type': 'application/json',
-		'Access-Control-Allow-Origin' : 'http://192.168.2.105:8080/Servlet',
+		//'Access-Control-Allow-Origin' : 'Servlet',
 	})
 }
 
 @Injectable()
 export class DashboardService {
-	url: string = 'http://192.168.2.105:8080/Servlet'; 
+	url: string = 'Servlet'; 
   constructor(private http: HttpClient) { }
 
   sendData(data: any){
@@ -21,5 +21,4 @@ export class DashboardService {
       catchError(val => of('I caught: ${val}'))
     );
   }
-
 }
